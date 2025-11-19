bb1220dd6f606264b3ca45dcc3ecd5d0ff82d025
Chay <cahyadi.nugraha@temadata.id>
2025-11-19 17:36:34 +0700

    feat: use single branch upon generating thru git

--- update.sh
+++ update.sh
@@ -8,14 +8,14 @@ if [ -z "$gkey" ]; then
     echo "Error: Google API key is required."
     exit 1
   fi
-  
+
   gkey=$GOOGLE_CALENDAR_API_KEY
 fi
 
-git clone git@github.com:chay22/liburday.git dist
+git clone --single-branch --branch main git@github.com:chay22/liburday.git dist
 ./bin/liburday-linux-amd64 --gkey $gkey --out-dir dist $year
 cd dist
-git add . 
+git add .
 git commit -m "feat: update"
 git push origin main
 cd ..
